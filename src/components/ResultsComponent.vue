0187c9bce9002ea48382678bb2a6ae25cc892995
BrenoFlag <117334417+BrenoFlag@users.noreply.github.com>
2025-06-12 02:14:52 -0400

    Appended the score output string

--- src/components/ResultsComponent.vue
+++ src/components/ResultsComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h3>{{score}}ms | {{score/1000}}s was your reaction time</h3>
-    <h3 v-if="showAverage">Your average reaction time is {{(reactionTimeSum/timesPlayed).toFixed(0)}}ms | {{(reactionTimeSum/timesPlayed/1000).toFixed(3)}}s</h3>
+    <h3>{{score}}ms | {{score/1000}}s was your reaction time, {{praise}}</h3>
+    <h3 v-if="showAverage">{{(reactionTimeSum/timesPlayed).toFixed(0)}}ms | {{(reactionTimeSum/timesPlayed/1000).toFixed(3)}}s is your average reaction time</h3>
   </div>
 </template>
 
@@ -10,12 +10,25 @@ export default {
   props:['score', 'reactionTimeSum', 'timesPlayed'],
   data(){
     return{
-      showAverage: false
+      showAverage: false,
+      praise: null
     }
   },
   methods:{
+    setpraise(){
+      if (this.score >= 400) this.praise = "Dude, wake up!"
+      else if (this.score >= 330) this.praise = "that was pretty slow!"
+      else if (this.score >= 370) this.praise = "that was slow!"
+      else if (this.score >= 240) this.praise = "that was average!"
+      else if (this.score >= 200) this.praise = "that was above average!"
+      else if (this.score >= 150) this.praise = "that was fast!"
+      else if (this.score >= 110) this.praise = "that was race car driver fast!"
+      else this.praise = "that was F1 driver fast!"
+
+    }
   },
   mounted() {
+    this.setpraise()
     if (this.timesPlayed > 1) {this.showAverage = true}
   }
 }
